ef3233e5480ad13e366108e57c97eee5ee4bc545
exantng <xiangyuan.tang@ericsson.com>
2019-03-11 09:52:52 +0800

    add GUARD for header

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 0.1.1 (2019-03-11)
+1. add GUARD for test-Vec.h
+
 ## 0.1.0 (2019-03-10)
 1. implement the customized Vec type
 1. add GUARD for all header files
--- ch11-defining-abstract-data-types/test-Vec.h
+++ ch11-defining-abstract-data-types/test-Vec.h
@@ -1,5 +1,10 @@
+#ifndef GUARD_test_Vec_h
+#define GUARD_test_Vec_h
+
 #include "Vec.h"
 
 int test_vec_int();
 int test_vec_char();
 int test_vec_students();
+
+#endif
