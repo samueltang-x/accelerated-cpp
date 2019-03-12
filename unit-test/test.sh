94d2f647f690c7d838794f14e6c233941215af58
exantng <xiangyuan.tang@ericsson.com>
2019-03-13 00:13:35 +0800

    update unit tet script to support execute from any dir

--- unit-test/test.sh
+++ unit-test/test.sh
@@ -1,4 +1,15 @@
-[ -f ./executeTests ] && rm -v ./executeTests
-cmake CMakeLists.txt
-make
-./executeTests
+#!/bin/bash
+
+function test() {
+  local fullPath=$(realpath $0)
+  local curDir=$(dirname ${fullPath})
+
+  cd $curDir
+  [ -f ./executeTests ] && rm ./executeTests
+  cmake CMakeLists.txt
+  make
+  ./executeTests
+  cd - > /dev/null
+}
+
+test
